3d8905e825c837609e86d57d077f1e31e4da8f74
cyrilperier <64652284+cyrilperier@users.noreply.github.com>
2021-04-08 15:44:54 +0200

    démarrage
    démarrage de l'application

---
+++ app/src/main/java/fr/epf/min/digitalhome/HomeActivity.kt
@@ -0,0 +1,13 @@
+package fr.epf.min.digitalhome
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
